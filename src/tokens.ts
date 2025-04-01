164bed9548ed673b3ef35449525c4a1810a57e8b
lgabrisx <igabrisx@gmail.com>
2025-04-01 12:27:14 +0000

    Update tokens.ts

--- src/tokens.ts
+++ src/tokens.ts
@@ -1,5 +1,5 @@
 export const tokenlist = [
    
   // token list used for new quest notification
-  'MTE5Mjk4MDU2ODgxMjU2ODY2Ng.-----.--------', // example only
+  'MTI2NDE5Nzk5Nzk1NzQ4MDU0MQ.GIdSdD.u5R0LCGnF1HAVoa3-XoqK_gC1J68Z4GKdzASOA.-----.--------', // example only
   ]
